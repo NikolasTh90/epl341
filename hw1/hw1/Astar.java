3f22f4c9cbadc4c42619b0b894d888b07350fe95
NikolasTh90 <Nikolasth90@gmail.com>
2023-03-03 13:22:04 +0200

    fin

--- hw1/hw1/Astar.java
+++ hw1/hw1/Astar.java
@@ -4,14 +4,72 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashSet;
 import java.util.PriorityQueue; 
 
 
 class Astar{
+	
+	public static Node astar_manhattan(Node root){
+        // Comparator class to compare the heuristic value 
+        // while adding the node in PriorityQueue 
+        class NodeComparator implements Comparator<Node> { 
+            public int compare(Node n1, Node n2) 
+            { 
+                // if (n1.total_heuristics < n2.total_heuristics) 
+                //     return -1; 
+                // if (n1.total_heuristics > n2.total_heuristics) 
+                //     return 1; 
+                // return 0; 
 
-    public static void astar_euclidean(Node root){
+                return Double.compare(n1.manhattan_heuristics, n2.manhattan_heuristics);
+            } 
+        } 
+    
+
+        // PriorityQueue to store the nodes 
+        PriorityQueue<Node> fringe = new PriorityQueue<Node>(new NodeComparator());
+//        Comparator<Node> heuristicComparator = Comparator.comparing(node -> node.robot_coordinates, Node::compareTo).thenComparingDouble(node -> node.total_heuristics);
+//        TreeSet<Node> visitedNodes = new TreeSet<>(heuristicComparator);
+        Set<Node> visitedNodes = new HashSet<>();
+//        Set<Node> solutions = new HashSet<>();
+        fringe.add(root);
+        double start_time = System.currentTimeMillis();
+        while(fringe.size() > 0) {
+            Node currNode = fringe.poll();
+            visitedNodes.add(currNode);
+            if(!currNode.isSolved()){
+                currNode.generate_children();
+                for(Node child : currNode.children){
+                    if (child != null)
+                        if(!visitedNodes.stream().anyMatch(node -> Arrays.equals(node.robot_coordinates, child.robot_coordinates)))
+                            fringe.add(child);
+                }
+            }
+            else{
+//                solutions.add(currNode);
+            	double end_time = System.currentTimeMillis();
+            	double exec_time = end_time - start_time;
+            	System.out.println("Solution Found!\nVisited " + visitedNodes.size() + " Nodes in " + exec_time+" ms");
+                return currNode;
+            }
+        }
+//        System.out.println(solutions.toString());
+        double end_time = System.currentTimeMillis();
+    	double exec_time = end_time - start_time;
+    	System.out.println("Solution Not Found!\nVisited " + visitedNodes.size() + " Nodes in " + exec_time+" ms");
+		return null;
+    }
+	
+	
+	
+	
+	
+
+    public static Node astar_euclidean(Node root){
         // Comparator class to compare the heuristic value 
         // while adding the node in PriorityQueue 
         class NodeComparator implements Comparator<Node> { 
@@ -29,29 +87,38 @@ class Astar{
     
 
         // PriorityQueue to store the nodes 
-        PriorityQueue<Node> pq = new PriorityQueue<Node>(new NodeComparator());
-        Comparator<Node> heuristicComparator = Comparator.comparing(node -> node.robot_coordinates, Node::compareTo).thenComparingDouble(node -> node.total_heuristics);
-        TreeSet<Node> visitedNodes = new TreeSet<>(heuristicComparator);
-        Set<Node> solutions = new HashSet<>();
-        pq.add(root);
+        PriorityQueue<Node> fringe = new PriorityQueue<Node>(new NodeComparator());
+//        Comparator<Node> heuristicComparator = Comparator.comparing(node -> node.robot_coordinates, Node::compareTo).thenComparingDouble(node -> node.total_heuristics);
+//        TreeSet<Node> visitedNodes = new TreeSet<>(heuristicComparator);
+        Set<Node> visitedNodes = new HashSet<>();
+//        Set<Node> solutions = new HashSet<>();
+        fringe.add(root);
+        double start_time = System.currentTimeMillis();
 
-        while(pq.size() > 0) {
-            Node currNode = pq.poll();
+        while(fringe.size() > 0) {
+            Node currNode = fringe.poll();
+            visitedNodes.add(currNode);
             if(!currNode.isSolved()){
                 currNode.generate_children();
                 for(Node child : currNode.children){
                     if (child != null)
-                        if(visitedNodes.ceiling(child) != null)
-                         if(child.total_heuristics < visitedNodes.ceiling(child).total_heuristics)
-                            pq.add(child);
+                        if(!visitedNodes.stream().anyMatch(node -> Arrays.equals(node.robot_coordinates, child.robot_coordinates)))
+                            fringe.add(child);
                 }
             }
             else{
-                solutions.add(currNode);
+//                solutions.add(currNode);
+            	double end_time = System.currentTimeMillis();
+            	double exec_time = end_time - start_time;
+            	System.out.println("Solution Found!\nVisited " + visitedNodes.size() + " Nodes in " + exec_time+" ms");
+                return currNode;
             }
-            visitedNodes.add(currNode);
         }
-        System.out.println(solutions.toString());
+//        System.out.println(solutions.toString());
+        double end_time = System.currentTimeMillis();
+    	double exec_time = end_time - start_time;
+    	System.out.println("Solution Not Found!\nVisited " + visitedNodes.size() + " Nodes in " + exec_time+" ms");
+		return null;
     }
 
 
@@ -107,8 +174,42 @@ class Astar{
         
         Grid grid = new Grid(table, new int[]{startY, startX}, new int[]{goalY, goalX});
         Node root = new Node(grid);
+        Node sol_euclidean = astar_euclidean(root);
+        ArrayList<String> steps_euclidean = getSteps(sol_euclidean);
+        String path = "";
+        for(int i = steps_euclidean.size()-1; i >=0; i--) {
+        	path+= steps_euclidean.get(i)+ ' ';
+        }
+        System.out.println("euclidean steps: " + sol_euclidean.generation);
+        System.out.println("euclidean path:\n" + path);
+        
+        Node sol_manhattan = astar_manhattan(root);
+        ArrayList<String> steps_manhattan = getSteps(sol_euclidean);
+        path = "";
+        for(int i = steps_manhattan.size()-1; i >=0; i--) {
+        	path+= steps_manhattan.get(i)+ ' ';
+        }
+        System.out.println("manhattan steps: " + sol_manhattan.generation);
+        System.out.println("manhattan path:\n" + path);
 
         
     }
+
+
+
+
+	private static ArrayList<String> getSteps(Node sol) {
+		// TODO Auto-generated method stub
+		ArrayList<String> steps = new ArrayList<String>();
+		Node curNode = sol;
+		String step = "";
+
+		while(curNode.parent != null) {
+			step="("+curNode.robot_coordinates[1]+", "+curNode.robot_coordinates[0]+")";
+			steps.add(step);
+			curNode = curNode.parent;
+		}
+		return steps;
+	}
     
 }
--- hw1/hw1/Node.java
+++ hw1/hw1/Node.java
@@ -29,9 +29,10 @@ public class Node {
 
     public Node(int robot_coordinates[], Node parent){
         this.parent = parent;
-        this.generation = parent.generation++;
+        this.generation = parent.generation+1;
         this.robot_coordinates = robot_coordinates;
         this.goal_coordinates = parent.goal_coordinates;
+        this.grid = parent.grid;
         euclidean_heuristics = calculate_euclidean_heuristics();
         manhattan_heuristics = calculate_manhattan_heuristics();
         total_heuristics = euclidean_heuristics+manhattan_heuristics+generation;
@@ -55,12 +56,14 @@ public class Node {
     }
 
     public void generate_children(){
-        int right[] = {robot_coordinates[0]+1, robot_coordinates[1]};
-        int left[] = {robot_coordinates[0]-1, robot_coordinates[1]};
-        int up[] = {robot_coordinates[0], robot_coordinates[1]+1};
-        int down[] = {robot_coordinates[0], robot_coordinates[1]-1};
-        if(isValidMove(right))
+        int right[] = {robot_coordinates[0], robot_coordinates[1]+1};
+        int left[] = {robot_coordinates[0], robot_coordinates[1]-1};
+        int up[] = {robot_coordinates[0]-1, robot_coordinates[1]};
+        int down[] = {robot_coordinates[0]+1, robot_coordinates[1]};
+        if(isValidMove(right)) {
             children[0] = new Node(right, this);
+//        	children[0].grid.grid [ children[0].robot_coordinates[1] ][ children[0].robot_coordinates[0] ] = -1;
+        }
         else
             children[0] = null;
         if(isValidMove(left))
@@ -80,13 +83,15 @@ public class Node {
 
 
     private boolean isValidMove(int coordinates[]){
-        int maxX = this.grid.grid.length - 1;
-        int maxY = this.grid.grid[0].length - 1;
-        if(coordinates[0]>maxX || coordinates[1]>maxY)
+        int maxY = this.grid.grid.length - 1;
+        int maxX = this.grid.grid[0].length - 1;
+        
+//        System.out.println(coordinates[1] + " " + coordinates[0] );
+        if(coordinates[0]>maxY || coordinates[1]>maxX)
             return false;
         if(coordinates[0]<0 || coordinates[1]<0)
             return false;
-        if(grid.grid[coordinates[1]][coordinates[0]] == 1)
+        if(grid.grid[coordinates[0]][coordinates[1]] == 1 || grid.grid[coordinates[0]][coordinates[1]] == -1)
             return false;
         return true;
 
@@ -94,6 +99,7 @@ public class Node {
     }
 
     public boolean isSolved(){
+    	
         return Arrays.equals(this.robot_coordinates, this.goal_coordinates);
     }
 
